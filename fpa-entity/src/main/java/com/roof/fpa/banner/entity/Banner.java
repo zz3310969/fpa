8c5809411847706aa4a47f1cbfddbc96bf374dd7
“lhttanner” <lht_ge@163.com>
2018-09-23 09:23:04 +0800

    fpa

--- fpa-entity/src/main/java/com/roof/fpa/banner/entity/Banner.java
+++ fpa-entity/src/main/java/com/roof/fpa/banner/entity/Banner.java
@@ -3,6 +3,7 @@ package com.roof.fpa.banner.entity;
 import javax.persistence.Id;
 import java.util.Date;
 import java.io.Serializable;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -10,56 +11,81 @@ import com.alibaba.fastjson.annotation.JSONField;
 
 /**
  * @author 模版生成 <br/>
- *         表名： c_banner <br/>
- *         描述：首页banner <br/>
+ * 表名： c_banner <br/>
+ * 描述：首页banner <br/>
  */
 @ApiModel(value = "c_banner", description = "首页banner")
 public class Banner implements Serializable {
-	// 需要手动添加非默认的serialVersionUID
-	@ApiModelProperty(value = "id")
-	protected Long id;// id
-	@ApiModelProperty(value = "跳转路径")
-	protected String path;// 跳转路径
-	@ApiModelProperty(value = "图片地址")
-	protected String imageUrl;// 图片地址
-	@ApiModelProperty(value = "类型")
-	protected String type;// 类型
+    // 需要手动添加非默认的serialVersionUID
+    @ApiModelProperty(value = "id")
+    protected Long id;// id
+    @ApiModelProperty(value = "跳转路径")
+    protected String path;// 跳转路径
+    @ApiModelProperty(value = "图片地址")
+    protected String imageUrl;// 图片地址
+    @ApiModelProperty(value = "类型")
+    protected String type;// 类型
+    @ApiModelProperty(value = "目标地址")
+    protected String target;//目标地址
+    @ApiModelProperty(value = "参数")
+    protected String params;//参数
 
-	public Banner() {
-		super();
-	}
 
-	public Banner(Long id) {
-		super();
-		this.id = id;
-	}
-	
-	@Id// 主键
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getPath() {
-		return path;
-	}
-	public void setPath(String path) {
-		this.path = path;
-	}
-	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-	
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
+    public Banner() {
+        super();
+    }
+
+    public Banner(Long id) {
+        super();
+        this.id = id;
+    }
+
+    @Id// 主键
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
 }
