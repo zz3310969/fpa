5d44969cb9b05880c27a85552a6962e8dd8b3c35
“lhttanner” <lht_ge@163.com>
2019-08-28 11:14:13 +0800

    fpa

--- fpa-wechat/src/main/java/com/roof/fpa/wechat/HomeController.java
+++ fpa-wechat/src/main/java/com/roof/fpa/wechat/HomeController.java
@@ -1,5 +1,8 @@
 package com.roof.fpa.wechat;
 
+import com.roof.fpa.account.service.api.IAccountService;
+import org.roof.spring.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +18,26 @@ import java.net.UnknownHostException;
  */
 @Controller
 public class HomeController {
+    @Autowired
+    private IAccountService accountService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public @ResponseBody String home(Model model, HttpServletRequest request) {
+    public @ResponseBody
+    String home(Model model, HttpServletRequest request) {
         try {
             String addr = InetAddress.getLocalHost().getHostAddress();
             String port = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
-            return "Hello Wold ip=" + addr+",prot:"+request.getLocalPort();
+            return "Hello Wold ip=" + addr + ",prot:" + request.getLocalPort();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
         return "Hello WeChat";
+    }
+
+    @RequestMapping(value = "usercount", method = {RequestMethod.GET})
+    public @ResponseBody
+    Result<Long> userCount() {
+        Long l = accountService.countAllUser();
+        return new Result(Result.SUCCESS, l);
     }
 }
